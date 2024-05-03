a34a1edfbbee987ed951b8cc828ce1b88c8999c3
fa18kouki <kouki0180@outlook.jp>
2024-05-03 12:45:15 +0900

    Env-Settings

--- app.py
+++ app.py
@@ -20,12 +20,22 @@ handler = WebhookHandler(LINE_CHANNEL_SECRET)
 openai.api_key = OPENAI_API_KEY
 # Google Cloud Storageの設
 GCS_BUCKET_NAME = os.getenv("GCS_BUCKET_NAME")
+# 環境変数から認証情報を取得
+credentials_dict = {
+    "type": os.getenv("TYPE"),
+    "project_id": os.getenv("PROJECT_ID"),
+    "private_key_id": os.getenv("PRIVATE_KEY_ID"),
+    "private_key": os.getenv("PRIVATE_KEY").replace('\\n', '\n'),
+    "client_email": os.getenv("CLIENT_EMAIL"),
+    "client_id": os.getenv("CLIENT_ID"),
+    "auth_uri": os.getenv("AUTH_URI"),
+    "token_uri": os.getenv("TOKEN_URI"),
+    "auth_provider_x509_cert_url": os.getenv("AUTH_PROVIDER_X509_CERT_URL"),
+    "client_x509_cert_url": os.getenv("CLIENT_X509_CERT_URL"),
+    "universe_domain": os.getenv("UNIVERSE_DOMAIN")
+}
 
-# GOOGLE_APPLICATION_CREDENTIALS環境変数からJSONファイルのパスを取得
-json_path = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS', './credentials.json')
-
-# JSONファイルから認証情報を読み込む
-credentials = service_account.Credentials.from_service_account_file(json_path)
+credentials = service_account.Credentials.from_service_account_info(credentials_dict)
 
 # Google Cloud Storageクライアントを初期化
 storage_client = storage.Client(credentials=credentials, project=credentials.project_id)
